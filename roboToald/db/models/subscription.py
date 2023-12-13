7ab4c8e89c37e3738904d7cddfd8a507627f9a13
Adam Harwell <flux.adam@gmail.com>
2023-12-13 02:06:21 -0800

    Notify once per window, allow custom lead time

--- roboToald/db/models/subscription.py
+++ roboToald/db/models/subscription.py
@@ -12,23 +12,29 @@ class Subscription(base.Base, base.MyBase):
 
     user_id = sqlalchemy.Column(sqlalchemy.Integer)
     target = sqlalchemy.Column(sqlalchemy.String(255))
-    expiry = sqlalchemy.Column(sqlalchemy.Integer)
-    last_notified = sqlalchemy.Column(sqlalchemy.Integer)
+    expiry = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    last_notified = sqlalchemy.Column(sqlalchemy.Integer, default=0)
+    lead_time = sqlalchemy.Column(sqlalchemy.Integer, default=1800,
+                                  nullable=False)
+    last_window_start = sqlalchemy.Column(sqlalchemy.Integer, default=0)
 
     # Cached for convenience
-    guild_id = sqlalchemy.Column(sqlalchemy.Integer)
+    guild_id = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
 
     __table_args__ = (
         sqlalchemy.PrimaryKeyConstraint(
             'user_id', 'target', name='pk_user_id_target'),
     )
 
-    def __init__(self, user_id, target, expiry, last_notified, guild_id):
+    def __init__(self, user_id, target, expiry, guild_id, lead_time,
+                 last_notified=0, last_window_start=0):
         self.user_id = user_id
         self.target = target
         self.expiry = expiry
         self.last_notified = last_notified
         self.guild_id = guild_id
+        self.lead_time = lead_time
+        self.last_window_start = last_window_start
 
 
 def get_subscription(user_id: int, target: str) -> Subscription:
@@ -52,17 +58,17 @@ def get_subscriptions_for_user(user_id: int) -> List[Subscription]:
 
 def get_subscriptions_for_notification() -> List[Subscription]:
     with base.get_session() as session:
-        subs = session.query(Subscription).filter(
-            Subscription.last_notified < time.time() - 31 * 60).all()
+        subs = session.query(Subscription).all()
     return subs
 
 
-def mark_subscription_sent(user_id: int, target: str) -> bool:
+def mark_subscription_sent(user_id: int, target: str, start_time: int) -> bool:
     with base.get_session() as session:
         sub: Subscription = session.get(
             Subscription, {"user_id": user_id, "target": target})
     try:
         sub.last_notified = int(time.time())
+        sub.last_window_start = start_time
         sub.store()
         return True
     except:
--- roboToald/discord_client/commands/cmd_raidtarget.py
+++ roboToald/discord_client/commands/cmd_raidtarget.py
@@ -47,8 +47,17 @@ async def subscribe(
         inter: disnake.ApplicationCommandInteraction,
         target: str = commands.Param(
             autocomplete=autocomplete_raid_target,
-            description="The name of the raid target")
+            description="The name of the raid target"),
+        lead_time_minutes: int = commands.Param(
+            default=0,
+            description="Number of minutes before the window to send the "
+                        "notification (additive with seconds)"),
+        lead_time_seconds: int = commands.Param(
+            default=0,
+            description="Number of seconds before the window to send the "
+                        "notification (additive with minutes)")
 ):
+    lead_time = int(lead_time_minutes * 60 + lead_time_seconds)
     if is_user_authorized(
             user_id=inter.user.id,
             guild_id=inter.guild_id,
@@ -58,8 +67,8 @@ async def subscribe(
             target=target,
             expiry=int(
                 time.time() + datetime.timedelta(days=30).total_seconds()),
-            last_notified=0,
-            guild_id=inter.guild_id
+            guild_id=inter.guild_id,
+            lead_time=lead_time
         )
         try:
             sub_db.store()
@@ -140,14 +149,15 @@ async def subscriptions(inter: disnake.ApplicationCommandInteraction):
 
 
 def make_subscription_embed(sub_obj: sub_model.Subscription) -> disnake.Embed:
+    last_notified = (f"<t:{sub_obj.last_notified}:R>"
+                     if sub_obj.last_notified else "Never")
     embed = disnake.Embed(
         title=sub_obj.target
     )
+    embed.add_field("Lead Time", "{:0>8}".format(
+        str(datetime.timedelta(seconds=sub_obj.lead_time))))
+    embed.add_field("Last Notification", last_notified)
     embed.add_field("Expires", f"<t:{sub_obj.expiry}:R>")
-    if sub_obj.last_notified:
-        embed.add_field("Last Notification", f"<t:{sub_obj.last_notified}:R>")
-    else:
-        embed.add_field("Last Notification", "Never")
     return embed
 
 
@@ -181,20 +191,12 @@ async def unsubscribe_listener(inter: disnake.MessageInteraction):
 
 
 def make_announce_embed(
-        active_window: rt_data.RaidWindow,
-        now: float) -> disnake.Embed:
+        active_window: rt_data.RaidWindow) -> disnake.Embed:
     embed = disnake.Embed()
     target = active_window.target
-    past_tense = active_window.get_status() == rt_data.RaidWindowStatus.NOW
     embed.title = f":boom:** {target.name} **:boom:"
     embed.add_field("Start", f"<t:{active_window.start}:R>")
     embed.add_field("End", f"<t:{active_window.end}:R>")
-    if past_tense:
-        percent = active_window.get_percent_elapsed(now)
-        # time_left = active_window.end - now
-        # time_left = datetime.timedelta(seconds=int(time_left))
-        embed.add_field(
-            "% Complete", f"`{percent * 100:.2f}%`")
     next_window = active_window.get_next()
     if next_window:
         embed.add_field("Next (estimated)", f"<t:{next_window.start}:R>")
@@ -214,16 +216,19 @@ async def announce_subscriptions():
 
     now = time.time()
     messages = []
-    for target in rt_data.RaidTargets.get_targets():
+    raid_targets = rt_data.RaidTargets.get_targets()
+    for target in raid_targets:
+        if target.name not in sub_map:
+            continue
         active_window = target.get_active_window(now)
         if not active_window:
             continue
-        status = active_window.get_status(now)
-        if (status <= rt_data.RaidWindowStatus.SOON and
-                (active_window.get_time_until(now) <
-                 datetime.timedelta(minutes=30))):
-            embed = make_announce_embed(active_window, now)
-            for sub in sub_map.get(target.name, []):
+        time_until = active_window.get_time_until(now)
+        embed = make_announce_embed(active_window)
+        for sub in sub_map[target.name]:
+            within_lead = time_until <= datetime.timedelta(seconds=sub.lead_time)
+            already_notified = sub.last_window_start == active_window.start
+            if within_lead and not already_notified:
                 if is_user_authorized(
                         user_id=sub.user_id, guild_id=sub.guild_id,
                         role_id=get_member_role(sub.guild_id)):
@@ -231,13 +236,22 @@ async def announce_subscriptions():
                     if user:
                         messages.append(user.send(embed=embed))
                         sub_model.mark_subscription_sent(
-                            sub.user_id, target.name)
+                            sub.user_id, target.name,
+                            start_time=active_window.start
+                        )
                     else:
                         print(f"Could not load user for DM: {sub.user_id}")
                 else:
                     print(f"User `{sub.user_id}` did not have the required "
                           f"role, removing watch `{target.name}`.")
                     sub_model.delete_subscription(sub.user_id, target.name)
+            # elif within_lead:
+            #     print(f'Already notified {sub.user_id} about this '
+            #           f'{target.name} window.')
+            # else:
+            #     ttn = time_until - datetime.timedelta(seconds=sub.lead_time)
+            #     print(f'Not yet time to notify about {target.name} for '
+            #           f'{sub.user_id} ({ttn} to notification)')
 
     await asyncio.gather(*messages)
     if messages:
